99c66c60d9817dc9cc22fb074949c3982bf11223
Perry <petreli1@ymail.com>
2022-07-08 13:14:59 +0100

    added media links

---
+++ src/media/images/index.js
@@ -0,0 +1,15 @@
+import whereWeEnd from './WhereWeEnd.png'
+import dreamingOfDetunedLove from './DreamingOfDetunedLove.png'
+import ethereal from './Ethereal.png'
+import lysergic from './Lysergic.png'
+import stormEP from './StormEP.png'
+
+const art = {
+    whereWeEnd: whereWeEnd,
+    dreamingOfDetunedLove: dreamingOfDetunedLove,
+    lysergic: lysergic,
+    ethereal: ethereal,
+    stormEP: stormEP
+}
+
+export default art
---
+++ src/media/music/index.js
@@ -0,0 +1,28 @@
+import whereWeEnd from './where we end/Where We End.flac';
+
+import slumber from './dreaming of detuned love/Slumber.flac';
+import yourCarelessEmbrace from './dreaming of detuned love/Your Careless Embrace.flac';
+import restlessThoughts from './dreaming of detuned love/Restless Thoughts.flac';
+import detunedLove from './dreaming of detuned love/Detuned Love.flac';
+import fallingIntoTheVoid from './dreaming of detuned love/Falling into the Void.flac';
+
+import lysergic from './lysergic/Lysergic.flac';
+
+import distressed from './ethereal/Distressed.flac';
+import impurity from './ethereal/Impurity.flac';
+import achlys from './ethereal/Achlys.flac';
+import ethereal from './ethereal/Ethereal.flac';
+
+import serene from './storm ep/Serene.flac';
+import cumulonimbus from './storm ep/Cumulonimbus.flac';
+import rainfaill from './storm ep/Rainfall.flac';
+import thunder from './storm ep/Thunder.flac';
+import dissipate from './storm ep/Dissipate.flac';
+
+const dreamingOfDetunedLove = [slumber, yourCarelessEmbrace, restlessThoughts, detunedLove, fallingIntoTheVoid]
+
+const etherealEP = [distressed, impurity, achlys, ethereal]
+
+const stormEP = [serene, cumulonimbus, rainfaill, thunder, dissipate]
+
+export { whereWeEnd, dreamingOfDetunedLove, lysergic, etherealEP, stormEP }

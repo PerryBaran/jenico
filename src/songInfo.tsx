1ea502be6d9cd41e201b72ad6cb85a0277adb669
Perry <petreli1@ymail.com>
2022-07-08 13:15:20 +0100

    added songInfo

---
+++ src/songInfo.tsx
@@ -0,0 +1,33 @@
+import art from './media/images/index'
+import { whereWeEnd, dreamingOfDetunedLove, lysergic, etherealEP, stormEP } from './media/music/index'
+
+const songInfo: any = [
+    {
+        title: 'Where We End',
+        art: art.whereWeEnd,
+        songs: [whereWeEnd]
+    },
+    {
+        title: 'Dreaming of Detuned Love',
+        art: art.dreamingOfDetunedLove,
+        songs: dreamingOfDetunedLove,
+    },
+    {
+        title: 'Lysergic',
+        art: art.lysergic,
+        songs: [lysergic]
+    },
+    {
+        title: 'Ethereal',
+        art: art.ethereal,
+        songs: etherealEP
+    },
+    {
+        title: 'Storm EP',
+        art: art.stormEP,
+        songs: stormEP
+    }
+
+]
+
+export default songInfo

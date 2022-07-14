9aad2ab8fc2977712b2f3cd7def4c999cc0d5bfe
Perry <petreli1@ymail.com>
2022-07-14 19:58:12 +0100

    swapped over to mp3 for songs

--- src/media/music/index.js
+++ src/media/music/index.js
@@ -1,23 +1,23 @@
-import whereWeEndSrc from './where we end/Where We End.flac';
+import whereWeEndSrc from './where we end/Where We End.mp3';
 
-import slumberSrc from './dreaming of detuned love/Slumber.flac';
-import yourCarelessEmbraceSrc from './dreaming of detuned love/Your Careless Embrace.flac';
-import restlessThoughtsSrc from './dreaming of detuned love/Restless Thoughts.flac';
-import detunedLoveSrc from './dreaming of detuned love/Detuned Love.flac';
-import fallingIntoTheVoidSrc from './dreaming of detuned love/Falling into the Void.flac';
+import slumberSrc from './dreaming of detuned love/Slumber.mp3';
+import yourCarelessEmbraceSrc from './dreaming of detuned love/Your Careless Embrace.mp3';
+import restlessThoughtsSrc from './dreaming of detuned love/Restless Thoughts.mp3';
+import detunedLoveSrc from './dreaming of detuned love/Detuned Love.mp3';
+import fallingIntoTheVoidSrc from './dreaming of detuned love/Falling into the Void.mp3';
 
-import lysergicSrc from './lysergic/Lysergic.flac';
+import lysergicSrc from './lysergic/Lysergic.mp3';
 
-import distressedSrc from './ethereal/Distressed.flac';
-import impuritySrc from './ethereal/Impurity.flac';
-import achlysSrc from './ethereal/Achlys.flac';
-import etherealSrc from './ethereal/Ethereal.flac';
+import distressedSrc from './ethereal/Distressed.mp3';
+import impuritySrc from './ethereal/Impurity.mp3';
+import achlysSrc from './ethereal/Achlys.mp3';
+import etherealSrc from './ethereal/Ethereal.mp3';
 
-import sereneSrc from './storm ep/Serene.flac';
-import cumulonimbusSrc from './storm ep/Cumulonimbus.flac';
-import rainfaillSrc from './storm ep/Rainfall.flac';
-import thunderSrc from './storm ep/Thunder.flac';
-import dissipateSrc from './storm ep/Dissipate.flac';
+import sereneSrc from './storm ep/Serene.mp3';
+import cumulonimbusSrc from './storm ep/Cumulonimbus.mp3';
+import rainfaillSrc from './storm ep/Rainfall.mp3';
+import thunderSrc from './storm ep/Thunder.mp3';
+import dissipateSrc from './storm ep/Dissipate.mp3';
 
 const whereWeEnd = [
     {

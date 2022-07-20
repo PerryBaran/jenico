97baeb9eac1e473d9e12ed2cd3da5f487d7c2bed
Perry <petreli1@ymail.com>
2022-07-20 19:35:17 +0100

    added local storage for storing volume

--- src/components/player/volume/Volume.tsx
+++ src/components/player/volume/Volume.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, RefObject, ChangeEvent} from 'react';
 import style from './volume.module.css';
 import { mediumVolume, lowVolume, muteVolume } from '../../../media/icons/index';
+import { getLocalStorage, populateStorage } from '../../../services/localStorage';
 
 function Volume(props: {audioRef: RefObject<HTMLAudioElement>}) {
     const { audioRef } = props
     const [volume, setVolume] = useState(0.5);
-    const [icon, setIcon] = useState(mediumVolume);
-
+    
     useEffect(() => {
         if (audioRef.current) {
             audioRef.current.volume = volume;
@@ -14,19 +14,23 @@ function Volume(props: {audioRef: RefObject<HTMLAudioElement>}) {
     }, [volume, audioRef]);
 
     useEffect(() => {
-        setIcon(()=> {
-            if (volume === 0) {
-                return muteVolume
-            }
-            if (volume < 0.5) {
-                return lowVolume
-            }
-            return mediumVolume
-        });
-    }, [volume]);
+        const getVolume = Number(getLocalStorage('volume'));
+        getVolume && setVolume(getVolume)
+    }, []);
+
+    const icon = () => {
+        if (volume === 0) {
+            return muteVolume
+        }
+        if (volume < 0.5) {
+            return lowVolume
+        }
+        return mediumVolume
+    };
 
     const changeVolume = (e: ChangeEvent<HTMLInputElement>) => {
         const currentVolume = Number(e.target.value)/100;
+        populateStorage('volume', currentVolume);
         setVolume(currentVolume);
     };
 
@@ -39,7 +43,7 @@ function Volume(props: {audioRef: RefObject<HTMLAudioElement>}) {
                         max={100}
                         defaultValue={volume * 100}
                         onChange={e => changeVolume(e)}/>
-                    <button><img src={icon} alt='volume'/></button>
+                    <button><img src={icon()} alt='volume'/></button>
                 </div>
     );
 }

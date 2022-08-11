d46ecd007992b4326647bfba3e84c57a20d2fd22
Perry <petreli1@ymail.com>
2022-08-11 12:52:43 +0100

    implemented classes for constructing songs/album info, with read only variables

--- src/media/music/index.ts
+++ src/media/music/index.ts
@@ -1,69 +1,39 @@
-const whereWeEnd = [
-    {
-        name: 'Where We End',
-        ref: 'Where We End/Where We End.mp3'
-    }
-];
+import { Songs } from '../../Interface';
+
+class Song implements Songs{
+    readonly name: string;
+    readonly ref: string;
+    constructor(name: string, ref: string) {
+        this.name = name
+        this.ref = ref
+    };
+};
+
+const whereWeEnd = [new Song('Where We End', 'Where We End/Where We End.mp3')];
 
 const dreamingOfDetunedLove = [
-    {
-        name: 'Slumber',
-        ref: 'Dreaming Of Detuned Love/Slumber.mp3'
-    },{
-        name: 'Your Careless Embrace',
-        ref: 'Dreaming Of Detuned Love/Your Careless Embrace.mp3'
-    },{
-        name: 'Restless Thoughts',
-        ref: 'Dreaming Of Detuned Love/Restless Thoughts.mp3'
-    },{
-        name: 'Detuned Love',
-        ref: 'Dreaming Of Detuned Love/Detuned Love.mp3'
-    },{
-        name: 'Falling into the Void',
-        ref: 'Dreaming Of Detuned Love/Falling into the Void.mp3'
-    }
+    new Song('Slumber', 'Dreaming Of Detuned Love/Slumber.mp3'), 
+    new Song('Your Careless Embrace', 'Dreaming Of Detuned Love/Your Careless Embrace.mp3'), 
+    new Song('Restless Thoughts', 'Dreaming Of Detuned Love/Restless Thoughts.mp3'),
+    new Song('Detuned Love', 'Dreaming Of Detuned Love/Detuned Love.mp3'),
+    new Song('Falling into the Void', 'Dreaming Of Detuned Love/Falling into the Void.mp3')
 ];
 
-const lysergic = [
-    {
-        name: 'Lysergic',
-        ref: 'Lysergic/Lysergic.mp3'
-    }
-];
+const lysergic = [new Song('Lysergic', 'Lysergic/Lysergic.mp3')];
 
 const ethereal = [
-    {
-        name: 'Distressed',
-        ref: 'Ethereal/Distressed.mp3'
-    },{
-        name: 'Impurity',
-        ref: 'Ethereal/Impurity.mp3'
-    },{
-        name: 'Achlys',
-        ref: 'Ethereal/Achlys.mp3'
-    },{
-        name: 'Ethereal',
-        ref: 'Ethereal/Ethereal.mp3'
-    }
+    new Song('Distressed', 'Ethereal/Distressed.mp3'),
+    new Song('Impurity','Ethereal/Impurity.mp3'),
+    new Song('Achlys', 'Ethereal/Achlys.mp3'),
+    new Song('Ethereal', 'Ethereal/Ethereal.mp3')
 ];
 
 const stormEP = [
-    {
-        name: 'Serene',
-        ref: 'Storm EP/Serene.mp3'
-    },{
-        name: 'Cumulonimbus',
-        ref: 'Storm EP/Cumulonimbus.mp3'
-    },{
-        name: 'Rainfall',
-        ref: 'Storm EP/Rainfall.mp3'
-    },{
-        name: 'Thunder',
-        ref: 'Storm EP/Thunder.mp3'
-    },{
-        name: 'Dissipate',
-        ref: 'Storm EP/Dissipate.mp3'
-    }
+    new Song('Serene', 'Storm EP/Serene.mp3'),
+    new Song('Cumulonimbus', 'Storm EP/Cumulonimbus.mp3'),
+    new Song('Rainfall', 'Storm EP/Rainfall.mp3'),
+    new Song('Thunder', 'Storm EP/Thunder.mp3'),
+    new Song('Dissipate', 'Storm EP/Dissipate.mp3')
 ];
 
 export { whereWeEnd, dreamingOfDetunedLove, lysergic, ethereal as etherealEP, stormEP };
--- src/media/songInfo.ts
+++ src/media/songInfo.ts
@@ -1,38 +1,51 @@
 import {art} from './images/index'
 import { whereWeEnd, dreamingOfDetunedLove, lysergic, etherealEP, stormEP } from './music/index'
-import { SongInfo } from '../Interface'
+import { Songs, SongInfo } from '../Interface'
 
-const songInfo: SongInfo[] = [
-    {
-        title: 'Where We End',
-        art: art.whereWeEnd,
-        songs: whereWeEnd,
-        hyperlink: 'https://distrokid.com/hyperfollow/jenico/where-we-end'
-    },
-    {
-        title: 'Dreaming of Detuned Love',
-        art: art.dreamingOfDetunedLove,
-        songs: dreamingOfDetunedLove,
-        hyperlink: 'https://distrokid.com/hyperfollow/jenico/dreaming-of-detuned-love'
-    },
-    {
-        title: 'Lysergic',
-        art: art.lysergic,
-        songs: lysergic,
-        hyperlink: 'https://distrokid.com/hyperfollow/jenico/lysergic-2'
-    },
-    {
-        title: 'Ethereal',
-        art: art.ethereal,
-        songs: etherealEP,
-        hyperlink: 'https://distrokid.com/hyperfollow/jenico/ethereal'
-    },
-    {
-        title: 'Storm EP',
-        art: art.stormEP,
-        songs: stormEP,
-        hyperlink: 'https://distrokid.com/hyperfollow/jenico/storm-ep'
-    }
+class Album implements SongInfo{
+    readonly title: string
+    readonly art: string
+    readonly songs: Songs[]
+    readonly hyperlink: string
+    constructor (title: string, art: string, songs: Songs[], link: string) {
+        this.title = title
+        this.art = art
+        this.songs = songs
+        this.hyperlink = link
+    };
+};
+
+const songInfo = [
+    new Album(
+        'Where We End',
+        art.whereWeEnd,
+        whereWeEnd,
+        'https://distrokid.com/hyperfollow/jenico/where-we-end'
+    ),
+    new Album(
+        'Dreaming of Detuned Love',
+        art.dreamingOfDetunedLove,
+        dreamingOfDetunedLove,
+        'https://distrokid.com/hyperfollow/jenico/dreaming-of-detuned-love'
+    ),
+    new Album(
+        'Lysergic',
+        art.lysergic,
+        lysergic,
+        'https://distrokid.com/hyperfollow/jenico/lysergic-2'
+    ),
+    new Album(
+        'Ethereal',
+        art.ethereal,
+        etherealEP,
+        'https://distrokid.com/hyperfollow/jenico/ethereal'
+    ),
+    new Album(
+        'Storm EP',
+        art.stormEP,
+        stormEP,
+        'https://distrokid.com/hyperfollow/jenico/storm-ep'
+    )
 ];
 
 export default songInfo

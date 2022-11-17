c839aef169f27d7ccaf2172d026229bb7e95e795
Perry <petreli1@ymail.com>
2022-11-17 17:03:22 +0000

    updated sendEmail to use async await syntax

--- src/services/emailJS.ts
+++ src/services/emailJS.ts
@@ -2,21 +2,18 @@ import emailjs from "emailjs-com";
 
 const sendEmail = async (content: HTMLFormElement | null) => {
   if (content) {
-    await emailjs
-      .sendForm(
+    try {
+      await emailjs.sendForm(
         "service_szxchwk",
         "template_ldjw82j",
         content,
         "kavY_LmpokDn2OKmV"
-      )
-      .then(
-        () => {
-          alert("Email sent successfully");
-        },
-        (error) => {
-          alert(`Oops, something went wrong: ${error.text}`);
-        }
       );
+      alert("Email sent successfully");
+    } catch (err) {
+      alert(`Oops, something went wrong`);
+      console.error(err);
+    }
   }
 };
 

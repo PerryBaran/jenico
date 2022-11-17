e4625ed6798a00a831d1f8a353af68ecf4e52615
Perry <petreli1@ymail.com>
2022-11-17 14:22:45 +0000

    added Home tests

---
+++ src/__tests__/components/home.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../../components/routes/home/Home";
+import socials from "../../media/socials";
+
+describe("Home", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Home />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("content", () => {
+    render(<Home />);
+
+    expect(screen.getByText(/jenico/i)).toBeInstanceOf(HTMLHeadingElement);
+    expect(screen.getByTestId("video")).toBeInTheDocument();
+
+    const links = screen.getAllByRole("link");
+    links.forEach((link, i) => {
+      const social = socials[i];
+      const image = screen.getByAltText(`${social.name} icon`);
+
+      expect(link).toHaveAttribute("href", social.href);
+      expect(image).toHaveAttribute("src", social.src);
+      expect(screen.getByText(social.name)).toBeInTheDocument();
+    });
+  });
+});
